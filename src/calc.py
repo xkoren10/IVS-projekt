5ebc1f645d1bf7e9430729539ce72da54978952b
Matej Hložek <xhloze02@vutbr.cz>
2021-03-26 14:13:50 +0100

    Initial commit - calc.py

--- src/calc.py
+++ src/calc.py
@@ -1,1 +1,15 @@
-//Python code
+"""
+@file: calc.py
+@brief: Processing script for computing the result of input
+@author: Matej Hložek, xhloze02, PyJaMa's
+@date: March/April 2021
+"""
+
+"""evaluate
+@brief: connecting function for GUI
+@type x: string 
+@param input: String to calculate
+@rtype: string
+@return: a result"""
+def evaluate(input: str):
+    pass
